fc1bb914f2aafa54f87b1e4998d7ecf9e9b708d8
Amjith Ramanujam <amjith@netflix.com>
2021-11-01 15:47:53 -0700

    Make the type declaration work.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,6 @@
+export = fuzzyfind
 type Options = {
   accessor?: (a: any) => string
   precision?: number
 }
 export declare function fuzzyfind<T>(input: string, collection: T[], options?: Options): T[]
-export declare function search(haystack: string): {for: (needle: string) => string}
-export declare function generateGrams(word: string, precision: number): string[]
