ba0faf6f8df09c9375d68bb2f3d3516a09ae0a1e
Amjith Ramanujam <amjith@netflix.com>
2021-11-01 13:50:07 -0700

    Add type definitions.

---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+export declare function fuzzyfind<T>(input: string, collection: T[], options: ): T[]
+export declare function search(haystack: string): string
+export declare function generateGrams(word: string, precision: number): string[]
