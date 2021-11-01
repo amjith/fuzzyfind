2dccb88a7c626c3ebe6abbbcb22b30ddccad6148
Amjith Ramanujam <amjith@netflix.com>
2021-11-01 14:05:13 -0700

    Improve types. Still failing

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,7 @@
-export declare function fuzzyfind<T>(input: string, collection: T[], options: ): T[]
-export declare function search(haystack: string): string
+type Options = {
+  accessor?: (a: any) => string
+  precision?: number
+}
+export declare function fuzzyfind<T>(input: string, collection: T[], options?: Options): T[]
+export declare function search(haystack: string): {for: (needle: string) => string}
 export declare function generateGrams(word: string, precision: number): string[]
